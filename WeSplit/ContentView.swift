ea3c2761e4fcce7261dacd7bc863fd307477e79c
Jacob Aberasturi <jacobaberasturi@gmail.com>
2023-01-11 17:02:14 -0500

    Add basic Ui and navigation menus

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -14,7 +14,40 @@ struct ContentView: View {
     
     let tipPercentages = [10, 15, 20, 25, 0]
     var body: some View {
-        Text("filler")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(1..<100) {
+                            if $0 == 1 {
+                                Text("\($0) Person")
+                            }
+                            else {
+                                Text("\($0) People")
+                            }
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much do you want to tip")
+                }
+                
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+        }
     }
 }
 
