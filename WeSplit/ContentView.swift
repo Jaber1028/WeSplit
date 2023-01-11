e123d8cf925e898f318fef0001175d041c8c7056
Jacob Aberasturi <jacobaberasturi@gmail.com>
2023-01-11 17:25:00 -0500

    Added calculate Tip

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,10 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 1
+    @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     
     let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 1)
+        let tipSelection = Double (tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
         NavigationStack {
             Form {
@@ -22,12 +34,8 @@ struct ContentView: View {
                     
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(1..<100) {
-                            if $0 == 1 {
-                                Text("\($0) Person")
-                            }
-                            else {
-                                Text("\($0) People")
-                            }
+                            $0 == 1 ? Text("\($0) Person") : Text("\($0) People")
+
                         }
                     }
                 }
@@ -44,7 +52,7 @@ struct ContentView: View {
                 }
                 
                 Section {
-                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
         }
